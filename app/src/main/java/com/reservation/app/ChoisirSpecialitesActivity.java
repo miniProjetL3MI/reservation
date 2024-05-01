89a759fde644a90bf5ab5167758366491caa53ef
remil <sissa@gmail.com>
2024-05-02 01:13:05 +0200

    changement de page 1

--- app/src/main/java/com/reservation/app/ChoisirSpecialitesActivity.java
+++ app/src/main/java/com/reservation/app/ChoisirSpecialitesActivity.java
@@ -10,4 +10,5 @@ public class ChoisirSpecialitesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choisir_specialites);
     }
+
 }
