0e42ac03fc4237704f1a0ffe233e39315e260ad1
remil <sissa@gmail.com>
2024-05-02 01:06:55 +0200

    changement de page 1

--- app/src/main/java/com/reservation/app/ChoisirSpecialitesActivity.java
+++ app/src/main/java/com/reservation/app/ChoisirSpecialitesActivity.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ChoisirSpecialitesActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choisir_specialites);
     }
