6a05b808448d53589c5e66e6f7596784b38b4073
miniProjetL3MI <miniprojet47@gmail.com>
2024-05-02 01:06:36 +0100

    bravoo

--- app/src/main/java/com/reservation/app/LoginActivity.java
+++ app/src/main/java/com/reservation/app/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.reservation.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,6 +26,20 @@ public class LoginActivity extends AppCompatActivity {
         btn=findViewById(R.id.bouttonLogin);
         newUser=findViewById(R.id.newUtilisateur);
 
-        btn.setOnClickListener(view -> Toast.makeText(getApplicationContext(),"Login Success",Toast.LENGTH_SHORT).show());
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username=edUserName.getText().toString();
+                String password=edPassword.getText().toString();
+                if(username.length()==0 || password.length()==0){
+                    Toast.makeText(getApplicationContext(), "Remplissez les champs s'il vous plait", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(), "Authentification validée", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this,ChoisirSpecialitesActivity.class));
+
+                }
+
+            }
+        });
     }
 }
