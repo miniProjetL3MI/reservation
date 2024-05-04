280b8bbc818c1ffb7420607a53daa9f88299a6e2
miniProjetL3MI <miniProjet47@email.com>
2024-05-04 19:50:52 +0200

    yas

--- app/src/main/java/com/reservation/app/ActivityChoisirMedecin.java
+++ app/src/main/java/com/reservation/app/ActivityChoisirMedecin.java
@@ -1,8 +1,17 @@
 package com.reservation.app;
 
-import android.os.Bundle;
+
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.content.Context;
+
+
+import android.widget.TextView;
+
+
 
 public class ActivityChoisirMedecin extends AppCompatActivity {
 
@@ -10,5 +19,14 @@ public class ActivityChoisirMedecin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choisir_medecin);
+        // Récupérer le nom du patient depuis les SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String nomPatient = sharedPreferences.getString("nom", "");
+
+        // Trouver le TextView dans la mise en page XML
+        TextView welcomeTextView = findViewById(R.id.welcomeTextView);
+
+        // Afficher le message de bienvenue avec le nom du patient
+        welcomeTextView.setText("Bienvenue, " + nomPatient + " !");
     }
 }
