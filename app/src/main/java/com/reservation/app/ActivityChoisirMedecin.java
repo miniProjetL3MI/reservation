8ec7c19c824d234df527aabf6663f2020dce7789
miniProjetL3MI <miniprojet47@gmail.com>
2024-05-04 18:14:04 +0200

    hhh

--- app/src/main/java/com/reservation/app/ActivityChoisirMedecin.java
+++ app/src/main/java/com/reservation/app/ActivityChoisirMedecin.java
@@ -1,14 +1,22 @@
 package com.reservation.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityChoisirMedecin extends AppCompatActivity {
-
+    ImageView btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choisir_medecin);
+        btn1 = findViewById(R.id.imageViewA);
+
+
+        btn1.setOnClickListener(v -> {
+            startActivity(new Intent(ActivityChoisirMedecin.this, ProfilCoteMedecinActivity.class));
+        });
     }
 }
