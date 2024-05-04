c64b514510f5e77fecd32b97c5d77c7ecb392c0f
miniProjetL3MI <miniprojet47@gmail.com>
2024-05-04 14:55:35 +0200

    vvv

--- app/src/main/java/com/reservation/app/ChoisirSpecialitesActivity.java
+++ app/src/main/java/com/reservation/app/ChoisirSpecialitesActivity.java
@@ -1,12 +1,45 @@
 package com.reservation.app;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChoisirSpecialitesActivity extends AppCompatActivity {
+    ImageView btn1, btn2, btn3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choisir_specialites);
-    }
 
+        btn1 = findViewById(R.id.imageView4);
+        btn2 = findViewById(R.id.imageView9);
+        btn3 = findViewById(R.id.imageView5);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ChoisirSpecialitesActivity.this, Medecin.class));
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ChoisirSpecialitesActivity.this, Medecin.class));
+
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ChoisirSpecialitesActivity.this, Medecin.class));
+
+            }
+        });
+    }
 }
