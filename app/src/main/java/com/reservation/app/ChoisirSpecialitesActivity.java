5b92ffd30ace2acd6184c2628ca926917b64732c
remil <sissa@gmail.com>
2024-05-04 19:38:55 +0200

    nom Patient

--- app/src/main/java/com/reservation/app/ChoisirSpecialitesActivity.java
+++ app/src/main/java/com/reservation/app/ChoisirSpecialitesActivity.java
@@ -1,24 +1,35 @@
 package com.reservation.app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 public class ChoisirSpecialitesActivity extends AppCompatActivity {
     ImageView btn1, btn2, btn3;
+    TextView nomPatient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choisir_specialites);
         btn1 = findViewById(R.id.imageView4);
+        nomPatient=findViewById(R.id.textView4);
 
         btn2 = findViewById(R.id.imageView9);
         btn3 = findViewById(R.id.imageView5);
 
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String nomP=sharedPreferences.getString("nom","testToto");
+
+        nomPatient.setText(nomP);
 
         btn1.setOnClickListener(v -> {
             startActivity(new Intent(ChoisirSpecialitesActivity.this, ActivityChoisirMedecin.class));
