c29cbdf6d00e3c1f98ef0db0d7b664af7b249cec
remil <sissa@gmail.com>
2024-05-05 15:49:05 +0200

    page specialite

--- app/src/main/java/com/reservation/app/ActivityChoisirMedecin.java
+++ app/src/main/java/com/reservation/app/ActivityChoisirMedecin.java
@@ -1,6 +1,8 @@
 package com.reservation.app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,31 +10,29 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityChoisirMedecin extends AppCompatActivity {
-    ImageView btn1,btn3;
-    TextView btn2 ,btn4;
+    TextView tv,nomPatient;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choisir_medecin);
-        btn1 = findViewById(R.id.imageViewA);
-        btn2=findViewById(R.id.textViewA);
-        btn3 = findViewById(R.id.imageView4);
-        btn4=findViewById(R.id.textView15);
 
-        btn1.setOnClickListener(v -> {
-            startActivity(new Intent(ActivityChoisirMedecin.this, ProfilMedecinCotePatient.class));
-        });
-        btn2.setOnClickListener(v -> {
-            startActivity(new Intent(ActivityChoisirMedecin.this, ProfilMedecinCotePatient.class));
-        });
-        btn3.setOnClickListener(v -> {
-            startActivity(new Intent(ActivityChoisirMedecin.this, ProfilMedecinCotePatient.class));
-        });
-        btn4.setOnClickListener(v -> {
-            startActivity(new Intent(ActivityChoisirMedecin.this, ProfilMedecinCotePatient.class));
-        });
+        tv=findViewById(R.id.textView6);
+        Intent it=getIntent();
+        String title=it.getStringExtra("title");
+        tv.setText(title);
+        nomPatient=findViewById(R.id.textView4);
+        nomPatient=findViewById(R.id.textView4);
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String nomP=sharedPreferences.getString("nom","testToto");
+        String prenomP=sharedPreferences.getString("prenom","testToto");
+
+        // Concaténer le nom et le prénom
+        String nomComplet = nomP + " " + prenomP;
+        nomPatient.setText(nomComplet);
+
+
 
     }
 }
